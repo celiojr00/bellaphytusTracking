f1270e443fb88cb984a4c26a3b7aa9e83d6c79bd
Celio Jr. <celioo.diasjr@gmail.com>
2024-03-21 18:02:51 -0300

    Start granlog

--- axios.js
+++ axios.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
     //baseURL: 'http://127.0.0.1:8000/',
-    baseURL: 'https://celeirobike.ddns.net/',
-    //baseURL: 'https://granlogprd.ddns.net/',
+    //baseURL: 'https://celeirobike.ddns.net/',
+    baseURL: 'https://granlogprd.ddns.net/',
     //baseURL: 'https://grupofesta.ddns.net/',
     //baseURL: 'https://grupojpp.ddns.net/',
     //baseURL: 'https://jsalog.ddns.net/',
