a89ab0771cf9575dffc33d987612353e9ebc7159
Celio Jr. <celioo.diasjr@gmail.com>
2024-03-21 18:03:53 -0300

    Start grupofesta

--- axios.js
+++ axios.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 const api = axios.create({
     //baseURL: 'http://127.0.0.1:8000/',
     //baseURL: 'https://celeirobike.ddns.net/',
-    baseURL: 'https://granlogprd.ddns.net/',
-    //baseURL: 'https://grupofesta.ddns.net/',
+    //baseURL: 'https://granlogprd.ddns.net/',
+    baseURL: 'https://grupofesta.ddns.net/',
     //baseURL: 'https://grupojpp.ddns.net/',
     //baseURL: 'https://jsalog.ddns.net/',
     //baseURL: 'https://macrolub.ddns.net/',
