07c87388aa980640b884557ade54675fceaa40a4
Celio Jr. <celioo.diasjr@gmail.com>
2024-03-22 08:47:32 -0300

    Start grupojpp

--- axios.js
+++ axios.js
@@ -11,7 +11,6 @@ const api = axios.create({
     //baseURL: 'https://officetotaleconopro.ddns.net/',
     //baseURL: 'https://ogcomercial.ddns.net/',
 
-    
 
     headers: {
         "Access-Control-Allow-Origin": "*",
