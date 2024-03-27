fa0e4554eea63ab4b52ac7217cf269c817a0cf10
Celio Jr. <celioo.diasjr@gmail.com>
2024-03-27 10:09:48 -0300

    commit Union

--- axios.js
+++ axios.js
@@ -5,7 +5,7 @@ const api = axios.create({
     //baseURL: 'https://celeirobike.ddns.net/',
     //baseURL: 'https://granlogprd.ddns.net/',
     //baseURL: 'https://grupofesta.ddns.net/',
-    baseURL: 'https://grupojpp.ddns.net/',
+    //baseURL: 'https://grupojpp.ddns.net/',
     //baseURL: 'https://jsalog.ddns.net/',
     //baseURL: 'https://macrolub.ddns.net/',
     //baseURL: 'https://officetotaleconopro.ddns.net/',
