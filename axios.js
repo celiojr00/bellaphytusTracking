fcb863fa4bea51b491880f8efe47d6ccafd4a736
Celio Jr. <celioo.diasjr@gmail.com>
2024-04-09 10:49:33 -0300

    commit bluevix

--- axios.js
+++ axios.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
     //baseURL: 'http://127.0.0.1:8000/',
+    baseURL: 'https://bluevix01.ddns.net/',
     //baseURL: 'https://celeirobike.ddns.net/',
     //baseURL: 'https://granlogprd.ddns.net/',
     //baseURL: 'https://grupofesta.ddns.net/',
