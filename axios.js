3fa328191c5b38af7211457cdd887ee870418ecb
Célio Jr <114538657+celiojr00@users.noreply.github.com>
2024-06-28 11:41:41 -0300

    Update axios.js

--- axios.js
+++ axios.js
@@ -7,14 +7,14 @@ const api = axios.create({
     //baseURL: 'https://globopharma2.ddns.net/',
     //baseURL: 'https://granlogprd.ddns.net/',
     //baseURL: 'https://grupofesta.ddns.net/',
-    //baseURL: 'https://grupojpp.ddns.net/',
+    baseURL: 'https://grupojpp.ddns.net/',
     //baseURL: 'https://jsalog.ddns.net/',
     //baseURL: 'https://lelloprint.ddns.net/',
     //baseURL: 'https://macrolub.ddns.net/',
     //baseURL: 'https://officetotaleconopro.ddns.net/',
     //baseURL: 'https://ogcomercial.ddns.net/',
     //baseURL: 'https://oncovit01.ddns.net/',
-    baseURL: 'https://vitascience.ddns.net/',
+    // baseURL: 'https://vitascience.ddns.net/',
 
 
     headers: {
