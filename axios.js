3dd1782708b5a90826686442a9afc603f030aab6
Célio Jr. <114538657+celiojr00@users.noreply.github.com>
2024-07-24 13:37:37 -0300

    Autenteicação via api_token

--- axios.js
+++ axios.js
@@ -25,6 +25,8 @@ const api = axios.create({
         'X-Requested-With': 'XMLHttpRequest',
         // 'Authorization': `Bearer 6|ONqlo215CHCtZbiSsyicPzhaPCow48ZcuKCVYgZp`
         // 'Authorization': `Bearer ` + localStorage.getItem("token"),
+
+        'Authorization': `Bearer 41|wdm5mpXBOiteT7OQ7Ga5upBCGMPy3qIWwGYx0f84`  //oncovit
     },
 })
 
