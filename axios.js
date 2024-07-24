798f78aa53f408f007132c942a37d8610fa548b1
Célio Jr. <114538657+celiojr00@users.noreply.github.com>
2024-07-24 13:39:05 -0300

    Autenteicação via api_token

--- axios.js
+++ axios.js
@@ -25,6 +25,8 @@ const api = axios.create({
         'X-Requested-With': 'XMLHttpRequest',
         // 'Authorization': `Bearer 6|ONqlo215CHCtZbiSsyicPzhaPCow48ZcuKCVYgZp`
         // 'Authorization': `Bearer ` + localStorage.getItem("token"),
+
+        'Authorization': `Bearer 75|C36jiKZRN0aJ5HGEwaHD43BYTlpKLFA9RNJokJaa`  // macrolub
     },
 })
 
