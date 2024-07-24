2b6552ec3a7ac19c65b5c3809172ec60f4b72535
Célio Jr. <114538657+celiojr00@users.noreply.github.com>
2024-07-24 13:45:14 -0300

    autenticação via api_token

--- axios.js
+++ axios.js
@@ -25,6 +25,8 @@ const api = axios.create({
         'X-Requested-With': 'XMLHttpRequest',
         // 'Authorization': `Bearer 6|ONqlo215CHCtZbiSsyicPzhaPCow48ZcuKCVYgZp`
         // 'Authorization': `Bearer ` + localStorage.getItem("token"),
+
+        'Authorization': `Bearer 37|nIJALPhiiCorCYweIo6a5u5IydiST83sDdfYdd0r`  // lelloprint
     },
 })
 
