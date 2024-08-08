82775a1ff3aebb92f8fb164798f763ede3e5fed2
Celio Jr. <celioo.diasjr@gmail.com>
2024-08-08 10:20:57 -0300

    Up novos clientes

--- axios.js
+++ axios.js
@@ -17,8 +17,8 @@ const api = axios.create({
     //baseURL: 'https://vitascience.ddns.net/',
     //baseURL: 'https://tetrix.ddns.net/',
     //baseURL: 'https://theramartbrasil.ddns.net/',
-    baseURL: 'https://qualiflex02.ddns.net/',
-    //baseURL: 'https://docesfardin.ddns.net/',
+    //baseURL: 'https://qualiflex02.ddns.net/',
+    baseURL: 'https://docesfardin.ddns.net/',
     //baseURL: 'https://bellaphytus.ddns.net/',
 
 
@@ -48,8 +48,8 @@ const api = axios.create({
         //'Authorization': `Bearer 100|UEePRO6279oSO6S28YOMxbOLacWFUN5EebI02gPF` // vitascience
         //'Authorization': `Bearer 23|8mTuUto8B6edqSAD6I6wic1flQCZXZDvL9BNcHEL`  // tetrix
         //'Authorization': `Bearer 7|NY3vHVnwj1ow5HzCHSnUyhHcp8FeCKBrIEdyJw9w`   // theramartbrasil
-        'Authorization': `Bearer 49|wfATErRpX4XkiVU6dj18KzqZAQnPjO50JjgTXnhD`  // qualiflex02
-        //'Authorization': `Bearer 4|s5zs0qYW6nCkPqnTFNLfuwvPQrQjoqGOgIZK1fvs`   // docesfardin
+        //'Authorization': `Bearer 49|wfATErRpX4XkiVU6dj18KzqZAQnPjO50JjgTXnhD`  // qualiflex02
+        'Authorization': `Bearer 4|s5zs0qYW6nCkPqnTFNLfuwvPQrQjoqGOgIZK1fvs`   // docesfardin
         //'Authorization': `Bearer 8|qrxSU7Thh9Op8eWxingK7vyzjLE04kBGXerIOTGx`   // bellaphytus
 
     },
