328f57a1b38c552e26041791f3a82e27b9f64c84
Celio Jr. <celioo.diasjr@gmail.com>
2025-02-14 13:41:32 -0300

    Chamado ID:579 - fisioquantic - retirar valor de nota do tracking

--- axios.js
+++ axios.js
@@ -26,8 +26,8 @@ const api = axios.create({
     // baseURL: 'https://creativecopias.econocargo.net/',
     // baseURL: 'https://menfirst.econocargo.net/',
     // baseURL: 'https://bapautomotiva.econocargo.net/',
-    // baseURL: 'https://fisioquantic.econocargo.net/',
-    baseURL: 'https://belfar.ddns.net/',
+    baseURL: 'https://fisioquantic.econocargo.net/',
+    // baseURL: 'https://belfar.ddns.net/',
     // baseURL: 'https://arellimpecom.ddns.net/',
     // baseURL: 'https://asclepharma.econocargo.net/',
     // baseURL: 'https://cgwbrasil.econocargo.net/',
@@ -87,8 +87,8 @@ const api = axios.create({
         // 'Authorization': `Bearer 5|F8rw4gt8wVvvcUgO76cRToNfd23kypk96lwcg10G`  // creative
         // 'Authorization': `Bearer 7|KTsMv4KlgCWG1vm2F7mNsrVLUbR535BMC80CaLi5`  // menfirst
         // 'Authorization': `Bearer 26|tDeBKJgqBUzBTNOnLpaYeQK2mCg7vpljDlwnMbLg` // barros pneus (bapautomotiva)
-        // 'Authorization': `Bearer 12|EbrbbHrTaL6khtvObXBtD3zRDkVyUJKsFT7k5iDZ` // fisioquantic
-        'Authorization': `Bearer 39|Utiey9x9qiPPlVDA3HLZza63lsnh6ZEWP0El8ZUm` // belfar
+        'Authorization': `Bearer 12|EbrbbHrTaL6khtvObXBtD3zRDkVyUJKsFT7k5iDZ` // fisioquantic
+        // 'Authorization': `Bearer 39|Utiey9x9qiPPlVDA3HLZza63lsnh6ZEWP0El8ZUm` // belfar
         // 'Authorization': `Bearer 3|qAXrboOs14eUeL6B8WaAAdXvW6nMNGpwj6fXYlY4`  // arell
         // 'Authorization': `Bearer 3|Qxjjb010WSqNiPCGknNsHHptJBAHCSi9tKeCHE9M`  // asclepharma
         // 'Authorization': `Bearer 4|uVuSmWpID1jdKbgBAfDtB6V57OIFbIQkAHMsfrWE`  // cgwbrasil
