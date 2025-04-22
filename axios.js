0f864b7f844528d51d58b4ef688e8a6009090afa
Celio Jr. <celioo.diasjr@gmail.com>
2025-04-22 17:27:35 -0300

    ajuste em icon logo whatsapp - grupofioforteTracking

--- axios.js
+++ axios.js
@@ -51,8 +51,8 @@ const api = axios.create({
     // baseURL: 'https://3mko.econocargo.net/',
     // baseURL: 'https://floranativabr.econocargo.net',
     // baseURL: 'https://arezes.econocargo.net',
-    // baseURL: 'https://grupofioforte.econocargo.net',
-    baseURL: 'https://jcdistribuidora.econocargo.net',
+    baseURL: 'https://grupofioforte.econocargo.net',
+    // baseURL: 'https://jcdistribuidora.econocargo.net',
     // baseURL: 'https://lotus.econocargo.net',
     // baseURL: 'https://bascel.econocargo.net/',
     // baseURL: 'https://lynv.econocargo.net/',
@@ -118,8 +118,8 @@ const api = axios.create({
         // 'Authorization': `Bearer 28|eoB5kLDheF91QiO12LnyWq3go2Yy4WBIDdbXoVHK` // 3mkocosmeticos
         // 'Authorization': `Bearer 5|4eRuH36y9rsb88YoT6LbZKBEvu6L6ThqtRhTUlhs`  // floranativabr
         // 'Authorization': `Bearer 8|ym38cTbQ0DVL8qZ1rBAV5vpf0BU7Z4lRwfEQ5z0N`  // arezes
-        // 'Authorization': `Bearer 3|ta51MtFPeASn3tepihH4d84WBh5eNtkp849YApry`  // grupofioforte
-        'Authorization': `Bearer 4|6DkvQBhCDmHykF4PiYXknOMDriPbuskXdwD1Lnbr`  // jcdistribuidora
+        'Authorization': `Bearer 3|ta51MtFPeASn3tepihH4d84WBh5eNtkp849YApry`  // grupofioforte
+        // 'Authorization': `Bearer 4|6DkvQBhCDmHykF4PiYXknOMDriPbuskXdwD1Lnbr`  // jcdistribuidora
         // 'Authorization': `Bearer 4|79ZcTww9eHMdVN5euv4DuOXxpGXVSgJipxPQPox9` // lotuscomercial
         // 'Authorization': `Bearer 3|sP5w1VXmsmfV7uwYP5gu3UC0HItQMTFd6b4gO0aA` // bascel
         // 'Authorization': `Bearer 4|YfPhBf4HSo6VmHHGYTfbCG05Z0Ts0iozZBZ5CLNJ` // lynv
