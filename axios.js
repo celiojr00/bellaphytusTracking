9c2207f0fd091ee3d44bb2a0fe098ad3541a0bf5
Celio Jr. <celioo.diasjr@gmail.com>
2025-04-23 08:28:20 -0300

    ajuste em icon logo whatsapp - grupofestaTracking

--- axios.js
+++ axios.js
@@ -6,8 +6,8 @@ const api = axios.create({
     // baseURL: 'https://celeirobike.ddns.net/',
     // baseURL: 'https://compose.ddns.net/',
     // baseURL: 'https://globopharma2.ddns.net/',
-    baseURL: 'https://granlogprd.ddns.net/',
-    // baseURL: 'https://grupofesta.ddns.net/',
+    // baseURL: 'https://granlogprd.ddns.net/',
+    baseURL: 'https://grupofesta.ddns.net/',
     // baseURL: 'https://grupojpp.ddns.net/',
     // baseURL: 'https://jsalog.ddns.net/',
     //  baseURL: 'https://jsalog01.econocargo.net/',
@@ -74,8 +74,8 @@ const api = axios.create({
         // 'Authorization': `Bearer 73|mZAIxTcPivg0TgWuyufRuNMlPrecRhOJhXpAU5NE` // bluevix
         // 'Authorization': `Bearer 62|8g6sZ0rUop6wsiwxWhzBaymJ4J9DkZtfDimtBykt` // celeirobike
         // 'Authorization': `Bearer 46|v75aZbw79NhvygdHLK9Qy4kpWEkbPgGnhn4F28XG` // globopharma
-        'Authorization': `Bearer 127|SOnmdzBkgD7R9a71ixekLqCqR90YS1aGA3vMNX2F`// granlog
-        // 'Authorization': `Bearer 54|rjbDlp9WctCSDkAYLNNBVQ0Itf3qwUakjqidbjG4` // grupofesta
+        // 'Authorization': `Bearer 127|SOnmdzBkgD7R9a71ixekLqCqR90YS1aGA3vMNX2F`// granlog
+        'Authorization': `Bearer 54|rjbDlp9WctCSDkAYLNNBVQ0Itf3qwUakjqidbjG4` // grupofesta
         // 'Authorization': `Bearer 128|v9JgUab08hJJ0w6p9cMYg5kLyf2qe8uiw77qUEyg`// grupojpp
         // 'Authorization': `Bearer 52|UXT7tt0WC7etnjpIDpQnLY1e4IP7xwiKZSjh2Zch` // jsalog
         // 'Authorization': `Bearer 3|ljvwaDfOJKyduyUDORwcbhdrPrjh6GIDva7nYqhg` // jsalog01
