fca61f5c9a96d1c140a5d94fad8e86095e4ebdb5
Vitor Moreira <vitormoreira2004@gmail.com>
2025-08-19 15:24:08 -0300

    mudança total no front do tracking

--- components/layout/Footer1.vue
+++ components/layout/Footer1.vue
@@ -1,97 +1,49 @@
 <template>
-  <footer class="main-footer" style="margin-top: -18ch; margin-bottom: 0ch ">
+  <footer class="main-footer" style="margin-top: 0ch; margin-bottom: 0ch ">
     <div class="bg-image" style="background-image: url(/images/background/5.jpg); margin-top: 5ch;"></div>
     <div class="anim-icons" style="position: absolute; margin-top: -2.9ch ;margin-left:70ch">
       <span class="icon icon-plane-3 bounce-x"></span>
     </div>
 
-    <!--Widgets Section-->
-<!--    <div class="widgets-section">
-      <div class="row justify-content-center">
-        <div class="d-flex">
-          &lt;!&ndash;Footer Column&ndash;&gt;
-          <div class="contact-info-block col-lg-2 col-md-4 col-sm-10 wow fadeInRight">
-            <div class="footer-widget about-widget">
-              <div class="logo" style="display: flex; align-items: center;">
-                <img src="/images/logoeconopro.png" style="max-width: 400px; margin-left: 10px;" alt="">
-              </div>
-            </div>
-          </div>
-
-          &lt;!&ndash;Footer Column&ndash;&gt;
-          <div class="contact-info-block col-lg-3 col-md-6 col-sm-12 wow fadeInRight" style="margin-right: 20px; margin-left: 20px;" data-wow-delay="250ms">
-            <div class="inner-box">
-              <div class="icon-box"><i class="icon flaticon-international-shipping-2"></i></div>
-              <h4 class="title">Endereço</h4>
-              <div class="text">Av. Eudes Scherrer de Souza, 2162 - Shopping Monserrat
-                Salas 713-717 - Colina de Laranjeiras - Serra/ES - CEP: 29167-080. </div>
-            </div>
-          </div>
-
-          &lt;!&ndash;Footer Column&ndash;&gt;
-          <div class="contact-info-block col-lg-2 col-md-6 col-sm-12 wow fadeInRight" style="margin-right: 20px;" data-wow-delay="500ms">
-            <div class="inner-box" >
-              <div class="icon-box"><i class="icon flaticon-24-hours-2"></i></div>
-              <h4 class="title mb-4">Horário de Atendimento</h4>
-              <div class="text">Segunda a Sexta de 08:00 a 18:00</div>
-            </div>
-          </div>
-
-          &lt;!&ndash;Footer Column&ndash;&gt;
-          <div class="contact-info-block col-lg-2 col-md-6 col-sm-12 wow fadeInRight" style="margin-right: 30px;" data-wow-delay="750ms">
-            <div class="inner-box">
-              <div class="icon-box"><i class="icon flaticon-stock-1"></i></div>
-              <h4 class="title mb-4">Contato</h4>
-              <div class="text">
-                <NuxtLink to="mailto:comercial@econocargo.com.br">comercial@econocargo.com.br</NuxtLink>
-                <br>
-                <NuxtLink to="tel:+55(27)3020-7888">(27) 3020-7888</NuxtLink>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>-->
-
     <!--Footer Bottom-->
     <div class="footer-bottom">
 <!--      <div class="auto-container">-->
-        <div class="inner-container">
-          <div class="contact-info-block col-lg-2 col-md-4 col-sm-10 wow fadeInRight">
-            <div class="footer-widget about-widget">
-              <div class="logo" style="display: flex; align-items: center;">
-                <img src="/images/logo_econoPro.png" style="max-width: 250px; margin-left: 30px;" alt="">
-              </div>
+      <div class="inner-container">
+        <div class="contact-info-block col-lg-2 col-md-4 col-sm-10 wow fadeInRight">
+          <div class="footer-widget about-widget">
+            <div class="logo" style="display: flex; align-items: center;">
+              <img src="/images/logo_econoPro.png" style="max-width: 250px; margin-left: 30px;" alt="">
             </div>
           </div>
+        </div>
 
-          <div class="copyright-text ms-auto" style="margin-right: 30px">
-            <p>&copy; Copyright 2024 by EconoPro <!--<NuxtLink to="/">Econocargo</NuxtLink>-->
-            </p>
+        <div class="copyright-text ms-auto" style="margin-right: 30px">
+          <p>&copy; Copyright 2024 by EconoPro <!--<NuxtLink to="/">Econocargo</NuxtLink>-->
+          </p>
 
 
-            <ul class="social-icon-two" style="margin-top: -25px ">
-              <li>
-                <NuxtLink to="#"><span class="fab fa-facebook-square" target="_blank" rel="noopener" no-rel></span></NuxtLink>
-              </li>
-              <li>
-                <NuxtLink to="#"><span class="fab fa-twitter" target="_blank" rel="noopener" no-rel></span></NuxtLink>
-              </li>
-              <li>
-                <NuxtLink to="#"><span class="fab fa-pinterest-p" target="_blank" rel="noopener" no-rel></span></NuxtLink>
-              </li>
-              <li>
-                <NuxtLink to="https://www.instagram.com/econocargo_web/" target="_blank" rel="noopener" no-rel><span class="fab fa-instagram"></span></NuxtLink>
-              </li>
-              <li>
-                <NuxtLink to="https://www.youtube.com/@econocargoconsultoria8877" target="_blank" rel="noopener" no-rel><span class="fab fa-youtube"></span></NuxtLink>
-              </li>
-            </ul>
-
-          </div>
-
+          <ul class="social-icon-two" style="margin-top: -25px ">
+            <li>
+              <NuxtLink to="#"><span class="fab fa-facebook-square" target="_blank" rel="noopener" no-rel></span></NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="#"><span class="fab fa-twitter" target="_blank" rel="noopener" no-rel></span></NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="#"><span class="fab fa-pinterest-p" target="_blank" rel="noopener" no-rel></span></NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="https://www.instagram.com/econocargo_web/" target="_blank" rel="noopener" no-rel><span class="fab fa-instagram"></span></NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="https://www.youtube.com/@econocargoconsultoria8877" target="_blank" rel="noopener" no-rel><span class="fab fa-youtube"></span></NuxtLink>
+            </li>
+          </ul>
 
         </div>
+
+
+      </div>
 <!--      </div>-->
     </div>
   </footer>
--- components/sections/home1/Section1.vue
+++ components/sections/home1/Section1.vue
@@ -1,5 +1,7 @@
 <template>
   <section class="banner-section">
+
+
     <swiper :slides-per-view="1" :space-between="30" :loop="true" :autoplay="{
             delay: 8000,
             disableOnInteraction: true,
@@ -14,8 +16,8 @@
 
       <!-- Slide Item -->
       <swiper-slide class="slide-item" >
-        <div class="bg-image" style="margin-top: -100px; background-image: url(/images/main-slider/3.jpg);"></div>
-        <div class="auto-container" style="margin-top: -40px;">
+        <div class="bg-image" style="background-image: url(/images/main-slider/3.jpg);"></div>
+        <div class="auto-container">
           <div class="row">
             <div class="content-column col-lg-7 col-md-12 col-sm-12">
 
@@ -57,53 +59,21 @@
               </div>
             </div>
 
-            <div class="content-column col-lg-5 col-md-12 col-sm-12">
-
-              <!--<div  class="content-box " style="position:absolute; top:-10ch; right: 1ch">
-                <figure class="image animate-5 animate-x"><img src="/images/resource/truck.png" alt="">
-                </figure>
-              </div>-->
+            <div style="margin-top: -17ch; margin-left: 46ch; position: relative;">
+              <div>
+                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+              </div>
             </div>
           </div>
         </div>
       </swiper-slide>
       <div  ref="targetRef"></div>
-      <!-- Slide Item -->
-      <!--            <swiper-slide class="slide-item">-->
-      <!--                <div class="bg-image" style="background-image: url(images/main-slider/3.jpg);"></div>-->
-      <!--                <div class="auto-container">-->
-      <!--                    <div class="row">-->
-      <!--                        <div class="content-column col-lg-7 col-md-12 col-sm-12">-->
-      <!--                            <div class="content-box">-->
-      <!--                                <h2 class="title animate-3">Most trusted logistics <span class="colored">&</span> delivery-->
-      <!--                                    services-->
-      <!--                                </h2>-->
-      <!--                                <div class="btn-box animate-4">-->
-      <!--                                    <NuxtLink to="/page-about" class="theme-btn btn-style-one alternate"><span-->
-      <!--                                            class="btn-title">Explore-->
-      <!--                                            More</span></NuxtLink>-->
-      <!--                                </div>-->
-      <!--                            </div>-->
-      <!--                        </div>-->
-      <!--                        <div class="image-column col-lg-5 col-md-12 col-sm-12">-->
-      <!--                            <div class="image-box">-->
-      <!--                                <div class="fact-counter-one bounce-y">-->
-      <!--                                    <h4 class="counter-title">Trusted by</h4>-->
-      <!--                                    <div class="count-box">-->
-      <!--                                        <ElementsCounterUp :end="4890" />-->
-      <!--                                    </div>-->
-      <!--                                </div>-->
-      <!--                                <figure class="image animate-5 animate-x"><img src="/images/resource/truck.png" alt="">-->
-      <!--                                </figure>-->
-      <!--                            </div>-->
-      <!--                        </div>-->
-      <!--                    </div>-->
-      <!--                </div>-->
-      <!--            </swiper-slide>-->
     </swiper>
 
 
   </section>
+
+
 </template>
 <script>
 import { Autoplay, Navigation, Pagination } from "swiper/modules"
