b56b23a1f438cf5ebd4cdf48bc8ab511d5178e27
Celio Jr. <celioo.diasjr@gmail.com>
2025-08-19 17:32:48 -0300

    update

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://granlogprd.ddns.net/',
+    baseURL: 'https://grupofesta.ddns.net/',
 
     headers: {
         "Access-Control-Allow-Origin": "*",
@@ -11,7 +11,7 @@ const api = axios.create({
         'Content-Type': 'application/json',
         'X-Requested-With': 'XMLHttpRequest',
 
-        'Authorization': 'Bearer 127|SOnmdzBkgD7R9a71ixekLqCqR90YS1aGA3vMNX2F'
+        'Authorization': 'Bearer 54|rjbDlp9WctCSDkAYLNNBVQ0Itf3qwUakjqidbjG4'
 
 
     },
