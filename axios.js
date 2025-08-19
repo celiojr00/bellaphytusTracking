ed5b264906d5c54b2dba0d3264ebc776abc7f306
Celio Jr. <celioo.diasjr@gmail.com>
2025-08-19 17:32:01 -0300

    update

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://atacadosaopaulo.ddns.net/',
+    baseURL: 'https://bapautomotiva.econocargo.net/',
 
     headers: {
         "Access-Control-Allow-Origin": "*",
@@ -11,7 +11,7 @@ const api = axios.create({
         'Content-Type': 'application/json',
         'X-Requested-With': 'XMLHttpRequest',
 
-        'Authorization': '23|ID00DByqzZOBVVe112ZXr2nJvNewyroYCs3YTJvF'
+        'Authorization': '26|tDeBKJgqBUzBTNOnLpaYeQK2mCg7vpljDlwnMbLg'
 
 
     },
