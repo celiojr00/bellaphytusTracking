e88d638d16e9fdf8eabf40523b0eec062b961164
Celio Jr. <celioo.diasjr@gmail.com>
2025-08-20 17:11:37 -0300

    update

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://orvelautomotor.ddns.net/',
+    baseURL: 'https://qualiflex02.ddns.net/',
 
     headers: {
         "Access-Control-Allow-Origin": "*",
@@ -11,7 +11,7 @@ const api = axios.create({
         'Content-Type': 'application/json',
         'X-Requested-With': 'XMLHttpRequest',
 
-        'Authorization': 'Bearer 13|wXdVWyPUMA6cjlOXfJVZrzg3vKLLvwtL6VBOOWsg'
+        'Authorization': 'Bearer 49|wfATErRpX4XkiVU6dj18KzqZAQnPjO50JjgTXnhD'
 
 
     },
