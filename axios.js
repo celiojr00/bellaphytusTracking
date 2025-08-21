ee237591aa42c8bcc4c8552b03466a342a66384d
Celio Jr. <celioo.diasjr@gmail.com>
2025-08-21 08:36:04 -0300

    update

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://grupofioforte.econocargo.net/',
+    baseURL: 'https://grupojpp.ddns.net/',
 
     headers: {
         "Access-Control-Allow-Origin": "*",
@@ -11,7 +11,7 @@ const api = axios.create({
         'Content-Type': 'application/json',
         'X-Requested-With': 'XMLHttpRequest',
 
-        'Authorization': 'Bearer 3|ta51MtFPeASn3tepihH4d84WBh5eNtkp849YApry'
+        'Authorization': 'Bearer 128|v9JgUab08hJJ0w6p9cMYg5kLyf2qe8uiw77qUEyg'
 
 
     },
