c353af2d2cec9296fcdd9cfd5faf6e05ea072463
Celio Jr. <celioo.diasjr@gmail.com>
2025-08-21 09:15:04 -0300

    update

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://tetrix.ddns.net/',
+    baseURL: 'https://theramartbrasil.ddns.net/',
 
     headers: {
         "Access-Control-Allow-Origin": "*",
@@ -11,7 +11,7 @@ const api = axios.create({
         'Content-Type': 'application/json',
         'X-Requested-With': 'XMLHttpRequest',
 
-        'Authorization': 'Bearer 23|8mTuUto8B6edqSAD6I6wic1flQCZXZDvL9BNcHEL'
+        'Authorization': 'Bearer 7|NY3vHVnwj1ow5HzCHSnUyhHcp8FeCKBrIEdyJw9w'
 
 
     },
