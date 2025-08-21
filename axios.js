6734b577414223840c50f7102079baa2ade885fe
Celio Jr. <celioo.diasjr@gmail.com>
2025-08-21 08:36:39 -0300

    update

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://officetotaleconopro.ddns.net/',
+    baseURL: 'https://ogcomercial.ddns.net/',
 
     headers: {
         "Access-Control-Allow-Origin": "*",
@@ -11,7 +11,7 @@ const api = axios.create({
         'Content-Type': 'application/json',
         'X-Requested-With': 'XMLHttpRequest',
 
-        'Authorization': 'Bearer 37|2QyK4bPGZtByEOvBKMXibNsUKaKeTPQI5WlpQfLw'
+        'Authorization': 'Bearer 7|ctmFJYjo0926YsRYw7aOGcNNiaYetGdJJBjzMr1i'
 
 
     },
