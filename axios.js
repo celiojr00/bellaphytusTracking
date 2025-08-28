e24c6b00843169aa029f5033ed3d1d1f8abcd41b
Celio Jr. <celioo.diasjr@gmail.com>
2025-08-28 16:38:17 -0300

    update

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://maifredo.econocargo.net/',
+    baseURL: 'https://majmobilidade.econocargo.net/',
 
     headers: {
         "Access-Control-Allow-Origin": "*",
@@ -11,7 +11,7 @@ const api = axios.create({
         'Content-Type': 'application/json',
         'X-Requested-With': 'XMLHttpRequest',
 
-        'Authorization': 'Bearer 3|fotdjjb6bdoivmDDVrnEAjOjirqdD6jXyz0oYp8H'
+        'Authorization': 'Bearer 10|5HZeg0luZDDsbbDjJVGGzDqq1xBP20Bi6sRGrad5'
 
 
     },
